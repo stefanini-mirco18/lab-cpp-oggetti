a3fc85e0f185539b87cff31944de1998218d4ab9
stefanini-mirco18 <stefaninimirco883@gmail.com>
2025-12-11 09:27:29 +0100

    sbagliato file

--- 1-string/1_string.cpp
+++ 1-string/1_string.cpp
@@ -1,101 +1,101 @@
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-/*
-    size() e length()
-    TODO: stampa la lunghezza della string passata in input
-*/
-void lunghezzaStringa(string stringa){
-
-    int x = stringa.length();
-    int y = stringa.size();
-    cout << "length: " << x << endl;
-    cout << "size: " << y << endl;
-
-}
-
-/*
-    find()
-    TODO: immettere una stringa da tastiera e comunicare all'utente se è presente la parola "ciao"
-            se la parola è presente, stampare "La parola ciao è presente"
-            altrimenti stampare "La parola ciao NON è presente"
-*/
-void trovaCiao(string stringa){
-
-   int x = stringa.find("ciao");
-   if(x==-1){
-    cout << "La parola ciao NON e' presente" << endl;
-   }else
-   cout << "La parola ciao e' presente" << endl;
-
-
-}
-
-
-/*
-    replace()
-    TODO: sostituire la parola "pizza" con la parola "pasta" e stampare la nuova stringa
-*/
-void sostituisciPizzaConPasta(string stringa){
-
-    int x = stringa.find("pizza");
-   if(x==-1){
-    cout << "La parola pizza NON e' presente" << endl;
-    cout << "la stringa e': " << stringa << endl;
-   }else{
-  stringa.replace(stringa.find("pizza"), 5, "pasta");
-  cout << "La parola pizza e' presente" << endl;
-    cout << "la stringa cambiata e': " << stringa << endl;
-    }
-
-
-
-
-}
-
-
-/*
-    substr()
-    TODO: estrarre e stampare la prima parola della stringa passata in input
-
-    HINT: la prima parola termina al primo spazio (trovabile con find())
-*/
-void stampaPrimaParola(string stringa){
-    // TODO
-    string s= stringa.substr(0, stringa.find(" "));
-    cout << "La prima parola e': " << s << endl;
-}
-
-
-/*
-    insert()
-    TODO: inserire alla fine della stringa passata in input " Giovanni!" e stampare la nuova stringa
-*/
-void aggiungiGiovanni(string frase){
-
-    frase.insert(frase.size(), " Giovanni!");
-    cout << "frase con giovanni: " << frase << endl;
-
-}
+/***************************************************************************
+ *                          Stringhe hands-on                             */
 
 int main()
 {
+    /******************************************************************************
+     * La classe è 'string' (in realtà std::string) e rappresenta il tipo di dato    
+     * Dichiarazione di oggetti di tipo string                                    */
 
-    string str;
+    string str1 = "Questa è una stringa";
+    string str2; //stringa vuota
+    string str3;
+    cout << "Inserire valore stringa str3: ";
+    getline(cin, str3);
 
-    cout << "Inserire valore stringa: ";
-    getline(cin, str);
+    
+
+    /*******************************************************************************************
+     *      Una stringa è semplicemente un array di char "nascosto"
+     *      Utilizzare la classe stringa, e non l'array di char, ne semplifica l'utilizzo      */
+    
+    str3[0]; //modo per accedere a ogni carattere della stringa (come un array di char)
+    str3.at(0); //modo object oriented per accedere a ogni carattere della stringa (come un array di char)
+
+    cout << "Primo carattere (posizione 0) " << str3[0] << endl; // stampa del primo carattere stringa str1 ("Questa è una stringa")
+    cout << "Primo carattere (posizione 0) " <<  str3.at(0) << endl; // stampa del primo carattere stringa str1 ("Questa è una stringa")
+    cout << endl;
 
 
-    lunghezzaStringa(str);
-    trovaCiao(str);
-    sostituisciPizzaConPasta(str);
-    stampaPrimaParola(str);
-    aggiungiGiovanni(str);
+
+    /********************************************************************************
+     * Ottenere informazioni sullo stato di un oggetto stringa attraverso funzioni */
+
+    // .size()
+    int x = str1.size();
+    cout << "La lunghezza della stringa è: " << x << endl;
+    cout << endl;
+    
+    // .length()
+    int y = str1.length();
+    cout << "La lunghezza della stringa è: " << y << endl;
+    cout << endl;
 
 
+    // .find()
+    int z = str1.find("Questa");
+    cout << "Stringa trovata all'indice: " << z << endl;
+    cout << endl;
+
+    // .substr()
+    string s = str1.substr(0, 6);
+    cout << "Nuova stringa estratta (da indice 0 a indice 5): " << s << endl;
+    cout << endl;
+
+
+    // .empty()
+    if(str2.empty())
+        cout << "Stringa vuota" << endl;
+    else cout << "Stringa non vuota" << endl;
+    cout << endl;
+
+
+
+
+    /****************************************************************************
+     *      Manipolare lo stato di un oggetto stringa attraverso funzioni      */
+
+    // .replace()
+    string str4 = "Non mi piace informatica";
+    cout << "Stringa prima di replace: " << str4 << endl;
+
+    str4.replace(13, str4.length(), "matematica");
+    cout << "Stringa dopo replace: " << str4 << endl;
+    cout<< endl;
+
+    
+    // .insert()
+    string str5 = "Questo è un";
+    cout << "Stringa prima di insert: " << str5 << endl;
+
+    str5.insert(str5.length(), " testo");
+    cout << "Stringa dopo insert: " << str5 << endl;
+    cout<< endl;
+
+    
+    // .erase()
+    string str6 = "Non mi piace informatica";
+    cout << "Stringa prima di erase: " << str6 << endl;
+
+    str6.erase(0, 4);
+    cout << "Stringa dopo erase: " << str6 << endl;
+    cout<< endl;
 
     return 0;
 }
