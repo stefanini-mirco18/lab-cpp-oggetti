9cbdcd5479f907fb169d90c97e738ac1ae0e547e
stefanini-mirco18 <249056654+stefanini-mirco18@users.noreply.github.com>
2026-01-10 11:57:48 +0100

    Non funz

--- 3-make/1_studente_basic.cpp
+++ 3-make/1_studente_basic.cpp
@@ -21,11 +21,18 @@ int main()
 {
     /*********************** Creazione di oggetti ******************************/
     // TODO Creare un nuovo studente "Mario" con eta 17 e media 7
+    Studente s1, *s2;
+    s1.nome = "Mario";
+    s1.eta = 17;
+    s1.media = 7;
     // TODO Creare un nuovo studente "Maria"  (questa volta con un puntatore) con eta 18 e media 8
-
-
+    s2->nome = "Maria";
+    s2->eta = 18;
+    s2->media = 8;
     /************************** Visualizzazione ********************************/
     // TODO Visualizzare il nome e la media sia di Mario che Maria
+    s1.stampaStudente();
+    s2->stampaStudente();
 
     return 0;
 }
