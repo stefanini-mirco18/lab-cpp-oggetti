311e81c0154be732380d76683f6b064967974244
stefanini-mirco18 <249056654+stefanini-mirco18@users.noreply.github.com>
2026-01-17 11:58:45 +0100

    Update 3_studente_cos.cpp

--- 3-make/3_studente_cos.cpp
+++ 3-make/3_studente_cos.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 /***********************************************************************************************************
- *                                 Modifica della classe studente (costruttore)                            *     
+ *                                 Modifica della classe studente (costruttore)                            *
  *     Quando creaimo un oggetto, non è molto comodo chiamare un metodo per ogni attributo da settare      *
  *     Ha senso creare una funzione che, in un colpo solo, assegna valori a tutti gli attributi            *
  *     Dato che è una cosa comune a tutte le classi che creiamo,                                           *
@@ -19,11 +19,18 @@ private:
 
 public:
     // Costruttore
-    Studente(string n, double e, float m): 
-        nome(n), 
+    Studente(string n, double e, float m):
+        nome(n),
         eta(e),
         media(m)
         {}
+
+    Studente(string n, double e):
+        nome(n),
+        eta(e),
+        media(999)
+        {}
+
 
     void setNome(string nuovoNome) {
         nome = nuovoNome;
@@ -47,7 +54,11 @@ public:
 
 int main()
 {
-    // TODO Istanziare un oggetto tramite il costruttore 
+    // TODO Istanziare un oggetto tramite il costruttore
+    Studente s1("GianLuigi", 18, 7.1);
+    s1.stampaStudente();
+    Studente s2("LeBonBon", 43);
+    s2.stampaStudente();
 
 
     return 0;
